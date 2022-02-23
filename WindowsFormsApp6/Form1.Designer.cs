7c722ad5c5a0d04f2029bb48cdd167aed7d894e1
youhacker55 <57035255+youhacker55@users.noreply.github.com>
2022-02-23 13:55:43 +0100

    Add files via upload

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "find admin panel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(9, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TargetWebsite";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.richTextBox1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 128);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(561, 144);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.richTextBox2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.richTextBox2.Location = new System.Drawing.Point(631, 24);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(153, 253);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(144, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "find emails";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::AdminPanelFinder.Properties.Resources._57035255__1_;
+            this.ClientSize = new System.Drawing.Size(809, 338);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Admin PanelFinder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using xNet;
+
+namespace WindowsFormsApp6
+{
+ 
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists("adminlink.txt"))
+            {
+                File.WriteAllText("adminlink.txt", AdminPanelFinder.Properties.Resources.adminlink);
+            }
+        }
+
+        public void check()
+
+
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+
+            {
+                MessageBox.Show("i don t see any url", "youhacker panel finder");
+                return;
+            }
+            if (!textBox1.Text.EndsWith("/")) textBox1.Text += "/";
+            if (!textBox1.Text.ToLower().StartsWith("http")) textBox1.Text = "http://" + textBox1.Text;
+
+            var results = "";
+            foreach (string line in File.ReadAllLines("adminlink.txt"))
+            {
+                richTextBox1.ReadOnly = true;
+                richTextBox1.AppendText($"\nURL:{textBox1.Text + line}    Checking:\n");
+                if (UrlIsValid(textBox1.Text + line))
+                {
+
+                    var results2 = "found this " + textBox1.Text + line+"\n";
+                    richTextBox1.AppendText(results2);
+                    using (StreamWriter file = new StreamWriter("results.txt"))
+                    {
+                        results = results + results2;
+                        file.Write(results);
+
+                    }
+
+               
+
+
+
+
+                }
+                else
+                {
+                    var fail = "[-]there is no admin Panel here " + textBox1.Text + line + "\n";
+                    richTextBox1.AppendText(fail);
+                }
+
+
+
+
+            }
+        }
+    
+        private static bool UrlIsValid(string url)
+        {
+            bool urlExists = false;
+            try
+            {
+                var httpRequest = new HttpRequest();
+                var response = httpRequest.Get(url);
+                if (response.StatusCode == xNet.HttpStatusCode.OK)
+                {
+                    urlExists = true;
+                    
+                }
+            }
+            catch { }
+            return urlExists;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(check);
+            thread.Start();
+
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (WebClient clone = new WebClient())
+            {
+               string cloned = clone.DownloadString(textBox1.Text);
+               string email = cloned;
+                string emails = "";
+                Regex reg = new Regex(@"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}", RegexOptions.IgnoreCase);
+                Match match;
+                for (match = reg.Match(email); match.Success; match = match.NextMatch())
+                {
+                    if (!(emails.Contains(match.Value)))
+                        emails = emails + match.Value + "\n";
+                        richTextBox2.Text = emails;
+             
+                }
+            }
+        }
+            
+            
+        }
+
+    }
+    
+
